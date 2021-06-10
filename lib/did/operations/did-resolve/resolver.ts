a4d4b0b63a69fc207ecbe7f2056c5f5d2af613ad
Tralcan <tralcanx@protonmail.com>
2021-06-10 20:18:17 -0300

    add init.tyron

--- lib/did/operations/did-resolve/resolver.ts
+++ lib/did/operations/did-resolve/resolver.ts
@@ -1,23 +1,28 @@
 /*
-    tyron.js: SSI Protocol's JavaScript/TypeScipt library
-    Self-Sovereign Identity Protocol.
-    Copyright (C) Tyron Pungtas and its affiliates.
+tyron.js: SSI Protocol's JavaScript/TypeScipt library
+Self-Sovereign Identity Protocol.
+Copyright (C) Tyron Pungtas and its affiliates.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-*/
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.*/
 
 import * as zcrypto from '@zilliqa-js/crypto';
 import SmartUtil from "../../../blockchain/smart-util";
 import ZilliqaInit from '../../../blockchain/zilliqa-init';
 import { NetworkNamespace } from '../../tyronzil-schemes/did-scheme';
+
+export enum InitTyron {
+    Testnet = "0x91e0a9Bd1af55E75a43b7F7608B9B8CB1b175F76",
+    Mainnet = "",
+    Isolated = ""
+}
 
 export default class Resolver {
 	public static async resolveDns(network: NetworkNamespace, initTyron: string, username: string, domain: string): Promise<string> {
