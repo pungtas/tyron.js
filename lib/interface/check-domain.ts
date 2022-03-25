9a3e072c7daad0e6f2485cacee7a27077e76671d
Ilham Bagas <ilhambagas@Ilhams-MacBook-Pro.local>
2022-03-25 12:57:33 +0700

    check domain

---
+++ lib/interface/check-domain.ts
@@ -0,0 +1,26 @@
+/*
+tyron.js: SSI Protocol's JavaScript/TypeScipt library
+Self-Sovereign Identity Protocol.
+Copyright (C) Tyron Pungtas and its affiliates.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.*/
+
+export default class CheckDomain {
+	constructor(
+		path: String
+	) {
+		if (path.split('.')[1] === 'did' || path.split('.')[1] === 'ssi' || path.split('.')[1] === 'vc' || path.split('.')[1] === 'treasury') {
+      return true
+    } else {
+      return false
+    }
+	}
+}
