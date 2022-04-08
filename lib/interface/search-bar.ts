a2deb2dff3d29b869b3bcb5d6738d0326b03eeca
Ilham Bagas <ilhambagas@Ilhams-MacBook-Pro.local>
2022-04-08 10:38:39 +0700

    fix check domain, check path

--- lib/interface/search-bar.ts
+++ lib/interface/search-bar.ts
@@ -13,20 +13,49 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.*/
 
-import CheckDomain from "./check-domain";
-import CheckPath from "./check-path";
-
 export default class SearchBar {
   
+  public static CheckDomain(path: String) {
+    if (
+      path.split(".")[1] === "did" ||
+      path.split(".")[1] === "ssi" ||
+      path.split(".")[1] === "vc" ||
+      path.split(".")[1] === "treasury"
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  public static CheckPath(path: String) {
+    if (path === "") {
+      return false;
+    } else if (path === "XPoints") {
+      return false;
+    } else if (
+      path.split("/")[1] === "did" ||
+      path.split("/")[1] === "xwallet" ||
+      path.split("/")[1] === "recovery" ||
+      path.split("/")[1] === "funds" ||
+      path.split("/")[1] === "buy" ||
+      path.split(".")[1] === "did" ||
+      path.split(".")[1] === "ssi" ||
+      path.split(".")[1] === "vc" ||
+      path.split(".")[1] === "treasury"
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
   public static SetDomain(path: String) {
-    const checkPath = new CheckPath(path);
-    const checkDomain = new CheckDomain(path);
-
     if (path.includes(".ssi")) {
       return "did";
-    } else if (checkPath) {
+    } else if (this.CheckPath(path)) {
       return "did";
-    } else if (checkDomain) {
+    } else if (this.CheckDomain(path)) {
       return path.split(".")[1];
     } else if (
       path.split("/")[1] === "did" ||
@@ -41,12 +70,10 @@ export default class SearchBar {
   }
 
   public static SetUsername(path: String) {
-    const checkPath = new CheckPath(path);
-    const checkDomain = new CheckDomain(path);
 
-    if (checkPath) {
+    if (this.CheckPath(path)) {
       return path;
-    } else if (checkDomain) {
+    } else if (this.CheckDomain(path)) {
       return path.split(".")[0];
     } else if (path.includes(".did") && path.includes("/")) {
       return path.split("/")[0].split(".")[0];
