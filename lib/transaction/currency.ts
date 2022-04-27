46eba4c76515d23c649a19b31207f01c5d3919c6
tralkan <tralkan@ssiprotocol.com>
2022-04-27 12:49:38 -0300

    remove addr name

--- lib/transaction/currency.ts
+++ lib/transaction/currency.ts
@@ -15,7 +15,6 @@ GNU General Public License for more details.*/
 
 export default class Currency {
   public static tyron(currency: String, input: number) {
-    let addr_name: string = "";
     let txID = "Transfer";
     let amount = 0;
 
@@ -26,73 +25,56 @@ export default class Currency {
         break;
       case "tyron":
         amount = input * 1e12;
-        addr_name = "tyron0";
         break;
       case "xcad":
         amount = input * 1e18;
-        addr_name = "xcad00";
         break;
       case "xsgd":
         amount = input * 1e6;
-        addr_name = "xsgd00";
         break;
       case "port":
         amount = input * 1e4;
-        addr_name = "port00";
         break;
       case "gzil":
         amount = input * 1e15;
-        addr_name = "gzil00";
         break;
       case "swth":
         amount = input * 1e8;
-        addr_name = "swth00";
         break;
       case "lunr":
         amount = input * 1e4;
-        addr_name = "lunr00";
         break;
       case "carb":
         amount = input * 1e8;
-        addr_name = "carb00";
         break;
       case "zwap":
         amount = input * 1e12;
-        addr_name = "zwap00";
         break;
       case "zusdt":
         amount = input * 1e6;
-        addr_name = "zusdt0";
         break;
       case "sco":
         amount = input * 1e4;
-        addr_name = "sco000";
         break;
       case "xidr":
         amount = input * 1e6;
-        addr_name = "xidr00";
         break;
       case "zwbtc":
         amount = input * 1e8;
-        addr_name = "zwbtc0";
         break;
       case "zeth":
         amount = input * 1e18;
-        addr_name = "zeth00";
         break;
       case "fees":
         amount = input * 1e4;
-        addr_name = "fees00";
         break;
       case "blox":
         amount = input * 1e2;
-        addr_name = "blox00";
         break;
     }
 
     const res = {
       txID,
-      addr_name,
       amount,
     };
 
